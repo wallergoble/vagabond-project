762dc5ae0481ada640849404f797ee11ce3d9ff2
Josh Owen <joshalldaylong@gmail.com>
2017-01-26 18:00:37 -0500

    changes before merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170126211645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "cities", force: :cascade do |t|
     t.string   "description"
@@ -38,5 +36,4 @@ ActiveRecord::Schema.define(version: 20170126211645) do
   end
 
   add_foreign_key "posts", "cities"
-
 end
