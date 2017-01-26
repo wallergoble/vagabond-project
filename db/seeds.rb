4c3f2c85d5641c811160d8035fd600d6bdbbcef9
Waller Goble <waller.go@gmail.com>
2017-01-26 16:54:11 -0500

    Working seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Clear the database before reseeding
+# City.delete_all
+# Post.delete_all
+
+# Initialize some cities
+atlanta = City.create description:"The city in the forest.", image_url:"http://www.diserio.com/atlantaskyline.jpg", city:"Atlanta", region:"Georgia", country:"United States of America"
+chicago = City.create description:"WINDY CITY BABY", image_url:"http://www.diserio.com/atlantaskyline.jpg", city:"Chicago", region:"Illinois", country:"United States of America"
+paris = City.create description:"Where cheese is made", image_url:"http://www.diserio.com/atlantaskyline.jpg", city:"Paris", region:"Centre", country:"France"
+
+# Initialize some posts
+atlanta_post_1 = Post.create author:"Swag", title:"Double Swag", body:"triple Swag", city_id:atlanta.id
+atlanta_post_2 = Post.create author:"Stuff", title:"Double stuff", body:"triple stuff", city_id:atlanta.id
+atlanta_post_3 = Post.create author:"Tips", title:"Double Tips", body:"triple Tips", city_id:atlanta.id
+
+chicago_post_1 = Post.create author:"Swag", title:"Double Swag", body:"triple Swag", city_id:chicago.id
+chicago_post_2 = Post.create author:"Stuff", title:"Double stuff", body:"triple stuff", city_id:chicago.id
+chicago_post_3 = Post.create author:"Tips", title:"Double Tips", body:"triple Tips", city_id:chicago.id
+
+paris_post_1 = Post.create author:"Swag", title:"Double Swag", body:"triple Swag", city_id:paris.id
+paris_post_2 = Post.create author:"Stuff", title:"Double stuff", body:"triple stuff", city_id:paris.id
+paris_post_3 = Post.create author:"Tips", title:"Double Tips", body:"triple Tips", city_id:paris.id
+
