ee7ec3de276bae3ee3d7f3081d861e87a044c9fa
Waller Goble <waller.go@gmail.com>
2017-01-26 22:17:08 -0500

    Fixed up post controller, untested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,27 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find params[:id]
+    end
+
+    def create
+        @city = City.find params[:city_id]
+        @post = @city.cities.create(post_params)
+
+        redirect_to city_path(@city)
+    end
+
+    def destroy
+        @city = City.find params[:city_id]
+        @post = @city.posts.find params[:id]
+
+        @post.destroy
+        redirect_to city_path(@city)
+    end
+
+    private
+    def post_params
+        params.require(:post).permit :body, :author, :title
+    end
+end
+
 end
